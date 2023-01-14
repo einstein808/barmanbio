dde7f57912e1c8c86a8022216e2682af63c51107
Gabryel Einstein de Carvalho Amaro <79322072+einstein808@users.noreply.github.com>
2023-01-13 23:51:08 -0300

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,13 +1,13 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
+        name: 'Adel Veículos',
         username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
+        url: 'adelveiculos.com.br',
         titleImg: true,
         avatar: '/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'Atendendo sempre com qualidade e atenção a nossos clientes. Nós, da Ádel Veículos, compramos, vendemos, trocamos e consignamos automóveis para melhor ... ',
         descShow: true,
         subdesc: 'Use this template and deploy on vercel for free',
         subdescShow: true,
