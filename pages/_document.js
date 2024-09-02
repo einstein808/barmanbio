248265df86af894535a5c660fd076274f89a532b
Einstein808 <gabryelamaroo@gmail.com>
2024-09-02 14:02:10 -0300

    atualizando googçe analyticas

--- pages/_document.js
+++ pages/_document.js
@@ -32,17 +32,19 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head> <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                                })(window,document,'script','dataLayer','GTM-P93HZJJ8');
+                <Head>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1CLL01F0PR"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+
+                                gtag('config', 'G-1CLL01F0PR');
                             `,
-                    }}
-                />
+                        }}
+                    />
 
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link
@@ -58,14 +60,14 @@ export default class MyDocument extends Document {
 
                 </Head>
                 <body>
-                <noscript>
+                    <noscript>
                         <iframe 
                             src="https://www.googletagmanager.com/ns.html?id=GTM-P93HZJJ8"
                             height="0"
                             width="0"
                             style={{ display: "none", visibility: "hidden" }}
                         ></iframe>
-                </noscript>
+                    </noscript>
 
                     <Main />
                     <NextScript />
