1006a16a04b125b56c8ba3a635e4208765dd397e
einstein808 <gabryel808einstein@gmail.com>
2024-09-02 13:03:55 -0300

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
@@ -30,8 +31,21 @@ export default class MyDocument extends Document {
 
     render() {
         return (
+            // eslint-disable-next-line react/jsx-no-comment-textnodes
             <Html>
-                <Head>
+                // eslint-disable-next-line @next/next/next-script-for-ga
+                <Head> <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-P93HZJJ8');
+                            `,
+                    }}
+                />
+
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link
                         rel="preconnect"
@@ -43,8 +57,17 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                     />
 
+
                 </Head>
                 <body>
+                <noscript>
+                        <iframe 
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-P93HZJJ8"
+                            height="0"
+                            width="0"
+                            style={{ display: "none", visibility: "hidden" }}
+                        ></iframe>
+                </noscript>
 
                     <Main />
                     <NextScript />
