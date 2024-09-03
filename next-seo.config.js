92615c5edfe7492383d0c5be00261bb9876d0ea8
einstein808 <gabryel808einstein@gmail.com>
2024-09-02 22:11:58 -0300

    atualizando seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,17 +1,19 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     openGraph: {
         type: 'website',
         locale: 'pt_br',
-        title: 'Gabryel Amaro',
-        url: 'Gabryel.Amaro',
-        description: 'Barman Matias Barbosa e Juiz de Fora',
+        title: 'Barman Bartender Gabryel Amaro',
+        url: 'https://barman.gabryelamaro.com',
+        description: 'Serviços de barman e bartender para festas e eventos em Matias Barbosa e Juiz de Fora. Gabryel Amaro oferece drinks exclusivos e atendimento personalizado.',
         locale: 'pt_br',
-        keywords: 'barman matias barbosa, barman juiz de fora, barman gabryel amaro',
+        keywords: 'barman matias barbosa, barman juiz de fora, barman gabryel amaro, bartender juiz de fora, bartender matias barbosa, barman para festa, barman para festa matias barbosa, barman para festa juizde fora',
         images: [
             {
                 width: 1200,
                 height: 630,
-                url: `gabryel.png`,
+                url: `https://i.imgur.com/CxrpKLy.jpeg`,
+                alt: 'Barman Gabryel Amaro preparando drinks em Matias Barbosa e Juiz de Fora'
             },
         ],
         site_name: 'Gabryel Amaro',
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://barman.gabryelamaro.com', // Substitua pelo domínio do seu site
+    generateRobotsTxt: true,        // (Opcional) Gera o robots.txt também
+  }
+  
